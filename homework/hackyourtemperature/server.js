b191c725c034d5667768dc3f75ff572c0be108b2
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-03-06 15:51:43 +0200

    finished project week1

---
+++ homework/hackyourtemperature/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello from backend to frontend!");
+});
+
+app.use(express.json());
+
+app.post("/weather", (req, res) => {
+  const cityName = req.body.cityName;
+  res.set("Content-Type", "application/json");
+  res.send(`You submitted: ${cityName}`);
+});
+
+app.listen(PORT, () => console.log(`server is listening now port ${PORT}`));

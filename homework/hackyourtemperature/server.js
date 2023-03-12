c548306d28c7edd46d7c2a19fb678b17aaee4d6d
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-03-12 21:30:42 +0200

    cityname and temp is done

--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import API_KEY from "./sources/keys.js";
+import { API_KEY } from "./sources/keys.js";
 import fetch from "node-fetch";
 
 const app = express();
@@ -11,17 +11,25 @@ app.get("/", (req, res) => {
 
 app.use(express.json());
 
-app.post("/weather", (req, res) async => {
-
-  try{
-   const respond= await fetch(`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid=${API_KEY}}`);
-  }catch(error){
-    console.log(error)
+app.post("/weather", async (req, res) => {
+  const { city } = req.body;
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+  );
+  const data = await response.json();
+  console.log(data);
+  
+  if (data.cod === "404") {
+    res.send({ weatherText: "City is not found!" });
+  } else {
+    const { name: cityName} = data;
+    const {main:{temp}} = data;
+    res.send({ weatherText: `The temperature in ${cityName} is ${temp}.` });
   }
 });
 
-app.listen(PORT, () => console.log(`server is listening now port ${PORT}`));
 
+app.listen(PORT, () => console.log(`server is listening now port ${PORT}`));
 
 // app.post("/weather", (req, res) => {
 //   const cityName = req.body.cityName;

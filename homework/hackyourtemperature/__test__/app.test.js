af946a3cfd8f034c7e161d785d4b81b87889254c
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-03-13 19:29:49 +0200

    test is done

--- homework/hackyourtemperature/__test__/app.test.js
+++ homework/hackyourtemperature/__test__/app.test.js
@@ -3,9 +3,38 @@ import supertest from "supertest";
 
 const request = supertest(app);
 
+describe("Given a correct city", () => {
+  it("It should respond with a 200 status code", async () => {
+    const response = await request.post("/weather").send({
+      city: "Boston"
+    });
+    expect(response.statusCode).toBe(200);
+  });
 
-describe("POST /", () => {
-  it("Quick test", () => {
-    expect(1).toBe(1);
+  it("should specify json in the content type header", async () => {
+    const response = await request
+      .post("/weather")
+      .send({ city: "Boston"});
+    expect(response.headers["content-type"]).toEqual(
+      expect.stringContaining("json")
+    );
   });
 });
+
+// Bad scenarios
+
+describe("Given a wrong or missing city ", () => {
+  it("It should respond with a negative message if city is wrong", async () => {
+    const response = await request.post("/weather").send({
+      city: "MissingCity"
+    });
+    expect(response.body.weatherText).toBe("City is not found!");
+  });
+
+  it("It should respond with a 404 status code if city is missing", async () => {
+    const response = await request.post("/weather").send({
+      city: "",
+    });
+    expect(response.statusCode).toBe(404);
+});
+})
--- homework/hackyourtemperature/server.js
+++ homework/hackyourtemperature/server.js
@@ -1,38 +1,4 @@
-import express from "express";
-import { API_KEY } from "./sources/keys.js";
-import fetch from "node-fetch";
+import app from "./app.js";
 
-const app = express();
 const PORT = 3000;
-
-app.get("/", (req, res) => {
-  res.send("hello from backend to frontend!");
-});
-
-app.use(express.json());
-
-app.post("/weather", async (req, res) => {
-  const { city } = req.body;
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-  );
-  const data = await response.json();
-  console.log(data);
-  
-  if (data.cod === "404") {
-    res.send({ weatherText: "City is not found!" });
-  } else {
-    const { name: cityName} = data;
-    const {main:{temp}} = data;
-    res.send({ weatherText: `The temperature in ${cityName} is ${temp}.` });
-  }
-});
-
-
 app.listen(PORT, () => console.log(`server is listening now port ${PORT}`));
-
-// app.post("/weather", (req, res) => {
-//   const cityName = req.body.cityName;
-//   res.set("Content-Type", "application/json");
-//   res.send(`You submitted: ${cityName}`);
-// });

0bb5ae411d6e60b97e4c0b7bfdba328b2a8e9706
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-03-18 14:16:03 +0200

    improve my codes after feedback

--- homework/hackyourtemperature/__test__/app.test.js
+++ homework/hackyourtemperature/__test__/app.test.js
@@ -3,38 +3,36 @@ import supertest from "supertest";
 
 const request = supertest(app);
 
-describe("Given a correct city", () => {
+describe("should respond with a success message if the city is found", () => {
   it("It should respond with a 200 status code", async () => {
     const response = await request.post("/weather").send({
-      city: "Boston"
+      city: "Boston",
     });
     expect(response.statusCode).toBe(200);
   });
 
   it("should specify json in the content type header", async () => {
-    const response = await request
-      .post("/weather")
-      .send({ city: "Boston"});
+    const response = await request.post("/weather").send({ city: "Boston" });
     expect(response.headers["content-type"]).toEqual(
-      expect.stringContaining("json")
+      "application/json; charset=utf-8"
     );
   });
 });
 
 // Bad scenarios
 
-describe("Given a wrong or missing city ", () => {
+describe("should respond with an error message if the city is not found", () => {
   it("It should respond with a negative message if city is wrong", async () => {
     const response = await request.post("/weather").send({
-      city: "MissingCity"
+      city: "MissingCity",
     });
     expect(response.body.weatherText).toBe("City is not found!");
   });
 
-  it("It should respond with a 404 status code if city is missing", async () => {
+  it("It should respond with a 400 status code if city is missing", async () => {
     const response = await request.post("/weather").send({
       city: "",
     });
-    expect(response.statusCode).toBe(404);
+    expect(response.statusCode).toBe(400);
+  });
 });
-})
--- homework/hackyourtemperature/app.js
+++ homework/hackyourtemperature/app.js
@@ -3,31 +3,27 @@ import { API_KEY } from "./sources/keys.js";
 import fetch from "node-fetch";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("hello from backend to frontend!");
 });
 
-app.use(express.json());
-
 app.post("/weather", async (req, res) => {
   const { city } = req.body;
-  console.log(city);
-
-  if (typeof city == "undefined" || city == "") {
-    res.status(404);
+  if (!city) {
+    res.status(400);
     res.send({ weatherText: "City is not found!" });
   } else {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     );
     const data = await response.json();
-
     if (data.cod === "404") {
       res.status(404);
       res.send({ weatherText: "City is not found!" });
     } else {
-      const { name: cityName } = data;
+      const cityName = data.name;
       const {
         main: { temp },
       } = data;
